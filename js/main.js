483a082537e7b4c7bec188714981cb0d66997efd
Michael Churchill <michaelchurchill@Michael.local>
2016-05-03 15:17:02 -0500

    no message

--- js/main.js
+++ js/main.js
@@ -27,7 +27,6 @@ var profileHTML = function(query) {
 		<h1 class="name">' + query.name + '</h1>\
 		<h2 class="login">' + query.login + '</h2>\
 		<HR>\
-		<ul></ul>\
 		<span class="list"><i class="material-icons md-18">room</i>' + query.location + '</span>\
 		<span class="list"><i class="material-icons md-18">mail_outline</i>' + query.email + '</span>\
 		<HR>'
@@ -41,7 +40,7 @@ var repoHTML = function(obj) {
 	var repoString = ""
 	var repoString = '<div class="repoDiv">\
 	    			 <span>' + obj.name + '</span>\
-                     <span class="rightRepo">JavaScript<i class="material-icons md-18">grade</i></span></div><HR>'
+                     <span class="rightRepo">JavaScript<i class="material-icons md-18">grade</i></span></div>'
 	repoContainer.innerHTML = repoString
 	return repoString
 }
